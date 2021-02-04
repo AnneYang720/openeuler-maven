05fd9538f7832701491f608c796f48db7730878e
Xiaoyan Yang <evangelin-lun@foxmail.com>
2021-02-04 10:55:27 +0800

    Add avatar.

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -4,15 +4,18 @@
     <breadcrumb></breadcrumb>
     <el-dropdown class="avatar-container" trigger="click">
       <div class="avatar-wrapper">
-        <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+        <!-- <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+         -->
+        <v-gravatar class="user-avatar" :email="email" alt="Avatar" default-img="identicon" />
+
         <i class="el-icon-caret-bottom"></i>
       </div>
       <el-dropdown-menu class="user-dropdown" slot="dropdown">
-        <el-dropdown-item>
-          <span @click="dialogVisible = true" type="text" size="small">个人设置</span>
+        <el-dropdown-item @click.native="dialogVisible = true" >
+          <span type="text" size="small">个人设置</span>
         </el-dropdown-item>
-        <el-dropdown-item divided>
-          <span @click="logout" style="display:block;">退出登录</span>
+        <el-dropdown-item @click.native="logout" divided>
+          <span style="display:block;">退出登录</span>
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
